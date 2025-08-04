739bbd2b0af3d03b3af46d089a13dffc2db43307
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-08-04 14:55:17 -0300

    Implementando lógica de repository

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -1,5 +1,20 @@
+import { prisma } from "@/lib/prisma"
+
+
+interface UserRequest{
+    name:string, 
+    email:string
+    password:string
+}
+
 class PrismaUsersRepository {
-    
+    async create(data:UserRequest) {
+        const user = await prisma.user.create({
+            data,
+        })
+        return user
+    }
+
 }
 
 export { PrismaUsersRepository }
